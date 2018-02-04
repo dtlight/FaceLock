d72bc94b2626f8a354d226e3c5bd2e8966230f0e
Stefan Dourado <dearz@hotmail.de>
2018-02-04 01:51:47 +0000

    Added encoding & decoding of images.

--- Server/app/routes.py
+++ Server/app/routes.py
@@ -1,8 +1,11 @@
-from flask import render_template, url_for, redirect, request
+from flask import render_template, url_for, redirect, request, abort
 from app import app, db, models
 from flask_user import login_required, current_user
 from app.models import User
 from werkzeug.utils import secure_filename
+import json
+import base64
+import os
 UPLOAD_FOLDER = 'static/images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -28,10 +31,16 @@ def unlock():
     return render_template('unlock.html')
 
 
-@app.route('/piTest', methods=["POST"])
-def method():
-    if 'file' not in (request.url):
-        if request.method == 'POST':
-            file = request.files['F']
-        print("1234")
-        return "HELLO"
+@app.route('/pi', methods=["POST"])
+def parse_request():
+    if not request.json:
+        abort(400)
+    img = request.get_json()
+    image_64_decode = ''
+    for key in img: 
+        image_64_decode = base64.b64decode(img[key])
+        abspath = os.path.abspath(os.path.dirname(__file__))
+        image_result = open(abspath + '/static/images/image.jpg','wb')
+        image_result.write(image_64_decode)
+        
+    return "Hi"
